ac32682b6dcd68e2bbcb6db1532dc87d086bc6fd
efrel <39706503+efrel@users.noreply.github.com>
2018-09-18 10:39:20 -0600

    mejoras de proveedores

---
+++ controller/user/proveedores.js
@@ -0,0 +1,37 @@
+app.controller('proveedores', function($scope, $rootScope, $http, $location) {
+    $rootScope.titulo = "Proveedor";
+    
+    $scope.guardarPro = function(dataPro){
+        console.log(dataPro);
+        $http({
+            method: "POST",
+            url: "services/saveProve.php",
+            data: $.param(dataPro),
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+            }
+        }).then(
+            function success(response) {
+                alert('Datos guardados correctamente');
+            },
+            function error(response) {
+                alert('Se produjo un error al cargar el proveedor.');
+            }
+        );
+    }
+
+    $http({
+        method: "POST",
+        url: "services/cargarTipServi.php",
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+      }).then(
+        function success(response) {
+            $scope.tipServi = response.data;
+        },
+        function error(response) {
+  
+    });
+
+});
