5c065cce81efa6470f25598ffcd8787f0456dd99
efrel <39706503+efrel@users.noreply.github.com>
2018-09-18 10:13:52 -0600

    guardar proveedores

--- services/saveProve.php
+++ services/saveProve.php
@@ -2,6 +2,5 @@
     include('includes/funciones.php');
 
     $sql2 = "INSERT INTO proveedor(nombre, ubicacion, contacto, telefono, correo, fk_tp) VALUES (?,?,?,?,?,?)";
-    insert($sql2, array($_POST['proveedor'], $_POST['ubicacion'], $_POST['contacto'], ,$_POST['numTel'], $_POST['correo'], ,$_POST['tipServi2']));
+    insert($sql2, array($_POST['proveedor'], $_POST['ubicacion'], $_POST['contacto'], $_POST['numTel'], $_POST['correo'], $_POST['tipServi2']));
 
-    
